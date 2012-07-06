94ce590135ba951c26a1a404bfe786a9f001f492
L. Donnie Smith <donnie.smith@gatech.edu>
2012-07-06 16:25:01 -0400

    Reorganize test.py

--- test.py
+++ test.py
@@ -1,24 +1,55 @@
-import rug, unittest
+import rug
+import unittest
+import os
+import shutil
 
-class RugTestCase(unittest.TestCase):
+test_url = 'git://github.com/abstrakraft/rug-test-project'
+test_repo = 'test_repo'
 
-	#TODO: Need a better explaination of the unit tests and what they accomplish. 
+class GitCloneTestCase(unittest.TestCase):
+	'''Test cases for git.Repo.clone'''
+	@classmethod
+	def tearDownClass(cls):
+		if os.path.exists(test_repo):
+			shutil.rmtree(test_repo)
 
-	url = 'git://github.com/abstrakraft/rug-test-project'
-
-	#test_cloneCheck is designed to check that the cloning operation works correctly thru checking the SHA's of the orginial repo against the cloned repo
-	def test_cloneCheck(self):
-		repo = rug.git.Repo.clone(self.url, 'test_repo')
+	def test_clone(self):
+		'''test_clone - test git.Repo.clone with vanilla arguments'''
+		repo = rug.git.Repo.clone(test_url, test_repo)
 		self.assertEqual(repo.rev_parse('remotes/origin/master'), repo.rev_parse('master'))
 
-	#test_revCheck is designed to check that the Rev command works correctly by checking that using the different avialable methods will generate the SHA's correctly
-	def test_revCheck(self):
-		repo=rug.git.Repo('test_repo')
+class GitRevTestCase(unittest.TestCase):
+	'''Test cases for git.Rev'''
+	@classmethod
+	def setUpClass(cls):
+		rug.git.Repo.clone(test_url, test_repo)
+
+	@classmethod
+	def tearDownClass(cls):
+		if os.path.exists(test_repo):
+			shutil.rmtree(test_repo)
+
+	def test_sha(self):
+		'''test_rev - test the shas returned by various incantations of git.Rev'''
+		repo=rug.git.Repo(test_repo)
 		head=repo.head()
 		self.assertEqual(rug.git.Rev(repo, head.get_short_name()).get_sha(), head.get_sha())
 		self.assertEqual(rug.git.Rev(repo, head.get_long_name()).get_sha(), head.get_sha())
 		self.assertEqual(rug.git.Rev(repo, head.get_sha()).get_sha(), head.get_sha())
 		self.assertTrue(rug.git.Rev(repo, head.get_sha()).is_sha())
 
-suite = unittest.TestLoader().loadTestsFromTestCase(RugTestCase)
+class ProjectCloneTestCase(unittest.TestCase):
+	'''Test cases for rug.Project.clone'''
+	@classmethod
+	def tearDownClass(cls):
+		if os.path.exists(test_repo):
+			shutil.rmtree(test_repo)
+
+	def test_clone(self):
+		'''test_clone - test rug.Project.clone with vanilla arguments'''
+		repo = rug.Project.clone(test_url, test_repo)
+
+suite = unittest.TestSuite()
+for case in [GitCloneTestCase, GitRevTestCase, ProjectCloneTestCase]:
+	suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(case))
 unittest.TextTestRunner(verbosity=2).run(suite)
